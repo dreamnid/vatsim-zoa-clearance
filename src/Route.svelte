50038b35d12ed2bfc4c01b5f42ac2187904f0640
Giovanni Go <dreamnid@dreamnid.com>
2023-08-01 22:59:54 -0700

    Add Route component

---
+++ src/Route.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import type { apt_info, star } from "./lib/ZOAAirports";
+
+  export let route: string;
+  export let airport: apt_info;
+
+  let star: star;
+
+  onMount(async () => {
+    let split = route.split(".");
+    let last = split[split.length - 1];
+    star = airport.arrival_proc.ifr?.stars.get(last);
+  });
+</script>
+
+<pre>{route}{#if star}{star.revision}{/if}</pre>
